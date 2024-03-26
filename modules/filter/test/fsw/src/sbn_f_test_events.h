1df84974e261e0144b52d6702e884157902decc4
Steve Kadlec <steven.kadlec@ga.com>
2024-03-26 12:30:37 -0600

    Move udp and filter modules

--- modules/filter/test/fsw/src/sbn_f_test_events.h
+++
@@ -1,8 +0,0 @@
-#ifndef _sbn_f_test_events_h
-#define _sbn_f_test_events_h
-
-#include "sbn_types.h"
-
-extern CFE_EVS_EventID_t SBN_F_TEST_FIRST_EID; /* defined at module init time */
-
-#endif /* _sbn_f_test_events_h */
