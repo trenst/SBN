5946c1f8c46eccb60987d3f2663962d7504905ca
Steve Kadlec <steven.kadlec@ga.com>
2024-04-18 12:03:26 -0600

    TCP now working

--- fsw/tables/sbn_conf_tbl.c
+++ fsw/tables/sbn_conf_tbl.c
@@ -1,11 +1,21 @@
 #include "sbn_tbl.h"
 #include "cfe_tbl_filedef.h"
 
-SBN_ConfTbl_t SBN_ConfTbl = {.ProtocolModules = {{/* [0] */
-                                                  .Name        = "UDP",
-                                                  .LibFileName = "/cf/sbn_udp.so",
-                                                  .LibSymbol   = "SBN_UDP_Ops",
-                                                  .BaseEID     = 0x0100}},
+SBN_ConfTbl_t SBN_ConfTbl = {.ProtocolModules = {
+                                                    // {/* [0] */
+                                                    //     .Name        = "UDP",
+                                                    //     .LibFileName = "/cf/sbn_udp.so",
+                                                    //     .LibSymbol   = "SBN_UDP_Ops",
+                                                    //     .BaseEID     = 0x0100
+                                                    // },
+                                                    {
+                                                        .Name        = "TCP",
+                                                        .LibFileName = "/cf/sbn_tcp.so",
+                                                        .LibSymbol   = "SBN_TCP_Ops",
+                                                        //sakdbg:  What should BaseEID be?
+                                                        .BaseEID     = 0x0200
+                                                    }
+                                                },
                              .ProtocolCnt     = 1,
                              .FilterModules   = {{/* [0] */
                                                 .Name        = "Remap",
@@ -20,27 +30,30 @@ SBN_ConfTbl_t SBN_ConfTbl = {.ProtocolModules = {{/* [0] */
                                       .ProcessorID  = 1,
                                       .SpacecraftID = 0x42,
                                       .NetNum       = 0,
-                                      .ProtocolName = "UDP",
+                                      .ProtocolName = "TCP",
                                       .Filters      = {"Remap"},
                                       .Address      = "127.0.0.1:2234",
                                       .TaskFlags    = SBN_TASK_POLL},
                                      {/* [1] */
-                                      .ProcessorID  = 2,
+                                      .ProcessorID  = 1,
                                       .SpacecraftID = 0x42,
                                       .NetNum       = 0,
-                                      .ProtocolName = "UDP",
+                                      .ProtocolName = "TCP",
                                       .Filters      = {"Remap"},
                                       .Address      = "127.0.0.1:2235",
                                       .TaskFlags    = SBN_TASK_POLL},
                                      {/* [2] */
-                                      .ProcessorID  = 3,
+                                      .ProcessorID  = 1,
                                       .SpacecraftID = 0x42,
                                       .NetNum       = 0,
-                                      .ProtocolName = "UDP",
+                                      .ProtocolName = "TCP",
                                       .Filters      = {"Remap"},
-                                      .Address      = "127.0.0.1:2236",
+                                      .Address      = "127.0.0.1:8774",
                                       .TaskFlags    = SBN_TASK_POLL},
                                  },
-                             .PeerCnt = 3};
+                             .PeerCnt = 3
+
+
+};
 
 CFE_TBL_FILEDEF(SBN_ConfTbl, SBN.SBN_ConfTbl, SBN Configuration Table, sbn_conf_tbl.tbl)
